617f34cc4c5485514c56ab9661ca4a04b0d767c2
benJervis <ben@jervis.net.au>
2017-06-02 11:39:40 +1000

    Updated icon

--- demopage/partials/header.php
+++ demopage/partials/header.php
@@ -2,5 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="/demopage/dashboard.css?v=6">
-<link rel="icon" type="image/x-icon" href="/demopage/favicon.ico">
+<link rel="icon" href="/demopage/logo-inverse.png">
 <title>CivitasCRM</title>
